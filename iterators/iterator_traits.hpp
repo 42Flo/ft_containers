8ea5db8942208afc1ead1c25c77e74b710fa87ca
42Flo <42flo@tuta.io>
2022-05-05 19:55:46 +0200

    all done. cleaning code.

--- iterators/iterator_traits.hpp
+++ iterators/iterator_traits.hpp
@@ -1,10 +1,13 @@
 #ifndef ITERATOR_TRAITS_HPP
 # define ITERATOR_TRAITS_HPP
 
+# include <cstddef>
+# include "../iterators/random_access_iterator.hpp"
+
 namespace ft
 {
 	template < class Iterator >
-	class iterator_traits
+	struct iterator_traits
 	{
 		public:
 			typedef typename Iterator::difference_type	    difference_type;
@@ -13,6 +16,26 @@ namespace ft
 			typedef typename Iterator::reference	        reference;
 			typedef typename Iterator::iterator_category    iterator_category;
 	};
+
+    template < class T >
+    struct iterator_traits<T*>
+    {
+        typedef ft::random_access_iterator<T, false>  iterator_category;
+        typedef std::ptrdiff_t  difference_type;
+        typedef T               value_type;
+        typedef T*              pointer;
+        typedef T&              reference;
+    };
+
+    template < class T>
+    struct iterator_traits<const T*>
+    {
+        typedef ft::random_access_iterator<T, true>  iterator_category;
+        typedef std::ptrdiff_t  difference_type;
+        typedef T               value_type;
+        typedef const T*        pointer;
+        typedef const T&        reference;
+    };
 }
 
 #endif // ITERATOR_TRAITS_HPP
